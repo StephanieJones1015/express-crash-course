110a30dbdc919bcc96dd762c56fcc318d7ccf595
Stephanie Jones <stephaniewilliams062715@gmail.com>
2025-07-13 20:01:26 -0400

    added posts.js and changed code from server.js to posts

--- server.js
+++ server.js
@@ -20,10 +20,11 @@ app.get('/api/posts', (req, res) => {
     const limit = parseInt(req.query.limit);
 
     if (!isNaN(limit) && limit > 0) {
-        res.status(200).json(posts.slice(0, limit));
-    } else {
-        res.status(200).json(posts);
-    }
+       return  res
+       .status(200)
+       .json(posts.slice(0, limit));
+    } 
+    res.status(200).json(posts);
 });
    
 //get single post
@@ -32,10 +33,11 @@ app.get('/api/posts/:id', (req, res) => {
     const post= posts.find((post) => post.id === id); 
 
     if(!post) {
-        res.status(404).json({msg: `A post with the id of ${id} was not found`});
-    } else { 
+     return res
+     .status(404)
+     .json({msg: `A post with the id of ${id} was not found`});
+    } 
         res.status(200).json(post);
-    }
 });
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
